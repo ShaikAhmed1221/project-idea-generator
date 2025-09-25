0890fec3b4d95104a0572c7088e58613222d8398
shaik ahmed <shaikahmed.ali1221@gmail.com>
2025-09-26 03:24:34 +0530

    backend changes3

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const generateRoute = require("./api/generate");
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
+
+// Route
+app.use("/api/generate", generateRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
